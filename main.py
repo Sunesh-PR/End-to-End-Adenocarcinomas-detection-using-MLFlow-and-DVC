42c1ccab930d3ddfe7be9f5521bda4e79581f497
suneshr7 <suneshchamp@gmai.com>
2024-02-28 21:26:30 +0530

    added requirements and logging

---
+++ main.py
@@ -0,0 +1,5 @@
+from classifierModel import logger
+
+
+
+logger.info('Start of the classifier!')
---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "End-to-End-Adenocarcinomas-detection-using-MLFlow-and-DVC"
+AUTHOR_USER_NAME = "Sunesh"
+SRC_REPO = "classifierModel"
+AUTHOR_EMAIL = "suneshr7@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for CNN app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
