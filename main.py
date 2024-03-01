d5bceaa285858c9c6c5caea1d47fddf6656ce540
suneshr7 <suneshchamp@gmai.com>
2024-03-01 11:48:06 +0530

    model trainer added

--- main.py
+++ main.py
@@ -1,16 +1,51 @@
 from classifierModel import logger
 from classifierModel.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from classifierModel.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from classifierModel.pipeline.stage_03_model_trainer import ModelTrainingPipeline
+
+
 
 
 STAGE_NAME = "Data Ingestion stage"
-if __name__ == '__main__':
-    try:
-        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataIngestionTrainingPipeline()
-        obj.main()
-        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-    except Exception as e:
-        logger.exception(e)
-        raise e
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 logger.info('Start of the classifier!')
+
+
+STAGE_NAME = "Prepare base model"
+
+
+
+
+
+
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = PrepareBaseModelTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+STAGE_NAME = "Training"
+
+try: 
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    model_trainer = ModelTrainingPipeline()
+    model_trainer.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
